23f39bf2d41eae5ef8874d8646a431e3e0d9204e
neoan <sroehrl@gmail.com>
2019-06-28 11:32:59 -0400

    add logic to establish ownership of article while respecting cache-plans

--- component/article/article.ce.js
+++ component/article/article.ce.js
@@ -3,7 +3,22 @@ new Vue({
     data:{
         isMine:false
     },
-    created(){
+    update(){
+        this.evaluateIfMine();
+    },
+    mounted(){
 
+        this.evaluateIfMine();
+    },
+    methods:{
+        evaluateIfMine(){
+            if(typeof localStorage.user !== 'undefined'){
+                let user = JSON.parse(localStorage.user);
+
+                let author = document.querySelector('#author-id');
+                this.isMine = author.value === user.id;
+            }
+
+        }
     }
 });
