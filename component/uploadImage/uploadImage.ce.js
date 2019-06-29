0a154bfee661817bda791f5c663a54e058aaf764
neoan <sroehrl@gmail.com>
2019-06-28 23:57:28 -0400

    Upload files size limitation

--- component/uploadImage/uploadImage.ce.js
+++ component/uploadImage/uploadImage.ce.js
@@ -1,21 +1,30 @@
 Vue.component('uploadImage', {
+    props:['maxSize'],
     data: function () {
         return {
+            sizeWarning:false,
             image:{}
         }
     },
     methods: {
         upload($event) {
-            let reader = new FileReader();
-            reader.readAsDataURL($event.target.files[0]);
-            reader.onload = (function (data) {
-                this.image = data.target.result;
-                api.post('uploadImage',{
-                    image:this.image
-                }).then(res=> {
-                    this.$emit('uploaded',res.data.uploadId);
-                })
-            }).bind(this);
+            if($event.target.files[0].size>Number(this.maxSize)*1024){
+                this.sizeWarning = true;
+            } else {
+                let reader = new FileReader();
+                reader.readAsDataURL($event.target.files[0]);
+                reader.onload = (function (data) {
+                    this.image = data.target.result;
+
+                    api.post('uploadImage',{
+                        image:this.image
+                    }).then(res=> {
+                        this.$emit('uploaded',res.data.uploadId);
+                    })
+                }).bind(this);
+            }
+
+
         }
     },
     template:document.querySelector('#uploadImage').innerHTML
