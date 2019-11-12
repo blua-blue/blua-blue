320e908835ceb024e113c53a0fe38b544dfa40b3
sroehrl <sroehrl@eagleproductivity.com>
2019-11-12 09:09:01 -0500

    add distinct login endpoint

--- component/login/Login.ctrl.php
+++ component/login/Login.ctrl.php
@@ -7,6 +7,7 @@ use Neoan3\Apps\Db;
 use Neoan3\Apps\Session;
 use Neoan3\Apps\Stateless;
 use Neoan3\Core\RouteException;
+use Neoan3\Core\Unicore;
 use Neoan3\Frame\Neoan;
 use Neoan3\Model\ImageModel;
 use Neoan3\Model\UserModel;
@@ -16,7 +17,16 @@ use Neoan3\Model\UserModel;
  *
  * @package Neoan3\Components
  */
-class Login extends Neoan {
+class Login extends Neoan
+{
+
+    function init()
+    {
+        $uni = new Unicore();
+        $uni->uni('neoan')->vueComponent('login')
+            ->hook('main', 'login')
+            ->output();
+    }
 
     /**
      * @param $credentials
@@ -25,23 +35,24 @@ class Login extends Neoan {
      * @throws RouteException
      * @throws \Neoan3\Apps\DbException
      */
-    function postLogin($credentials) {
+    function postLogin($credentials)
+    {
         $user = UserModel::login($credentials);
 
         // prepare roles/scopes
         $scope = ['user'];
-        if($user['user_type'] !== 'user'){
+        if ($user['user_type'] !== 'user') {
             $scope[] = $user['user_type'];
         }
 
         // attach image
-        if(!empty($user['image_id'])){
+        if (!empty($user['image_id'])) {
             $user['image'] = ImageModel::byId($user['image_id']);
         }
 
-        $jwt = Stateless::assign($user['id'],$scope,['exp'=>time()+(2*60*60*1000)]);
+        $jwt = Stateless::assign($user['id'], $scope, ['exp' => time() + (2 * 60 * 60 * 1000)]);
         // For hybrid auth
-        Session::login($user['id'],$user['roles'],$user['user_type']);
+        Session::login($user['id'], $user['roles'], $user['user_type']);
         $redirect = false;
         if (isset($_COOKIE['redirect'])) {
             $redirect = $_COOKIE['redirect'];
@@ -55,7 +66,8 @@ class Login extends Neoan {
     /**
      * void
      */
-    function deleteLogin(){
+    function deleteLogin()
+    {
         Session::logout();
     }
 
