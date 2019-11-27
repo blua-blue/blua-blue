8a191fa970268ad1c62bbcfae1f2497de0ecbfeb
neoan <sroehrl@gmail.com>
2019-11-26 21:23:20 -0500

    Modal-improvements: add two callback events & pass-through object

--- component/bluaModal/bluaModal.ce.js
+++ component/bluaModal/bluaModal.ce.js
@@ -1,23 +1,41 @@
 Vue.component('bluaModal', {
-    data: function(){
+    data: function () {
         return {
-            isActive:false,
-            content:'No content provided',
-            modalClass:'is-link',
-            headline:'Note'
+            isActive: false,
+            confirmable:false,
+            content: 'No content provided',
+            modalClass: 'is-link',
+            headline: 'Note',
+            object: {}
         }
     },
-    mounted(){
-        this.$root.$on('toggleModal',(args)=>{
-            Object.keys(args).forEach(arg =>{
+    mounted() {
+        this.$root.$on('toggleModal', (args) => {
+            Object.keys(args).forEach(arg => {
                 this[arg] = args[arg];
             });
             this.toggle();
         })
     },
-    methods:{
-        toggle:function(){
+    methods: {
+        confirm: function () {
+            this.$root.$emit('modalConfirmed',this.object);
+            this.toggle();
+        },
+        deny: function () {
+            this.$root.$emit('modalDenied',this.object);
+            this.toggle();
+        },
+        toggle: function () {
             this.isActive = !this.isActive;
+            if(!this.isActive){
+                // flush/reset
+                this.confirmable = false;
+                this.content = 'not content provided';
+                this.modalClass = 'is-link';
+                this.headline = 'Note';
+                this.object = {};
+            }
         }
     },
     template: document.querySelector('#bluaModal')
