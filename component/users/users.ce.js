a21c5c790e7abbb132a482129c45c108baebdd50
neoan <stefan.roehrl@gmail.com>
2021-10-18 17:45:30 -0400

    email verification & admin user view

--- component/users/users.ce.js
+++ component/users/users.ce.js
@@ -36,14 +36,26 @@ Vue.component('userList',{
             this.pagination.skip = (page-1) * this.pagination.top;
             api.get('users',{params:this.pagination}).then(res=>{
                 this.users = res.data.users;
-                this.pagination.total = res.total;
+                this.pagination.total = res.data.total;
                 this.calcPagination();
                 console.log(this.pagination);
             })
         },
         calcPagination(){
             this.pagination.links = [];
-            let buffer = [1,2,3];
+            const pages = Math.ceil(this.pagination.total / this.pagination.top);
+            console.log(pages)
+            if(this.currentPage > 1){
+                this.previousButton = true;
+            }
+            if(this.currentPage < pages.length){
+                this.nextButton = true;
+            }
+
+            for(let i = 0; i < pages; i++){
+                this.pagination.links.push({page:i + 1,active: i+1 === this.currentPage});
+            }
+            /*let buffer = [1,2,3];
             buffer.forEach(x=>{
                 if(this.currentPage - x > 0){
                     this.previousButton = true;
@@ -56,7 +68,7 @@ Vue.component('userList',{
                     this.pagination.links.push({page:this.currentPage + x,active:false});
                     this.nextButton = true;
                 }
-            })
+            })*/
         }
     },
     template:document.querySelector('#users').innerHTML
